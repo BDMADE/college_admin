7315709c91a0c5534582c81894e542f983483be8
hmtanbir <tanbir2025@gmail.com>
2016-09-18 23:09:29 +0600

    avater precompile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,4 +1,5 @@
 Rails.application.config.assets.precompile += %w( favicon_1.ico
+                                                  users/avatar-1.jpg
                                                   style.css
                                                   jquery.app.js
                                                   jquery.core.js

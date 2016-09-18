94410d3f2191c7210d604e6ffd927f7ce0dfd781
hmtanbir <tanbir2025@gmail.com>
2016-09-18 21:46:33 +0600

    assets folder done

--- spec/college_admin_spec.rb
+++ spec/college_admin_spec.rb
@@ -1,11 +1,11 @@
-require "spec_helper"
+require 'spec_helper'
 
 describe CollegeAdmin do
-  it "has a version number" do
+  it 'has a version number' do
     expect(CollegeAdmin::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it 'does something useful' do
+    expect(true).to eq(true)
   end
 end

d9f984271bd2eca36863ccb48b49ca637f24da82
hmtanbir <tanbir2025@gmail.com>
2016-09-18 23:08:06 +0600

    js precompile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,1 +1,5 @@
-Rails.application.config.assets.precompile += %w( favicon_1.ico )
+Rails.application.config.assets.precompile += %w( favicon_1.ico
+                                                  style.css
+                                                  jquery.app.js
+                                                  jquery.core.js
+                                                  jquery.dashboard.js )
